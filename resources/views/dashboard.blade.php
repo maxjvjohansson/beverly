1e1d341c5ce6cb7f1a999561c3a376dc56d407c6
Sandra <sandra0924@protonmail.com>
2025-03-07 22:03:16 +0100

    tweaks to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,10 +2,8 @@
 
     <section class="dashboard">
 
-        <h2>Welcome back <strong>{{ $user->name }}</strong>, to beverly dashboard!</h2>
+        <h2>Welcome back <strong>{{ $user->name }}</strong> to beverly dashboard!</h2>
         <p>Behind every great drink is a great admin</p>
-        
-        <img src="{{ asset('icons/drink.svg') }}" alt="Beverly logotype" />
 
     </section>
 
