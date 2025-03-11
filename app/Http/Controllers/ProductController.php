f1a7759e428942a73828c43510523a97ad328842
Sandra <sandra0924@protonmail.com>
2025-03-11 17:28:40 +0100

    cleaned up a line of code and deleted migration for jobs that wasn't in use

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Models\Category;
-use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
